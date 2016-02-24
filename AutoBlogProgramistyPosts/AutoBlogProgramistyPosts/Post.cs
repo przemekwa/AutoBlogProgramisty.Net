1c1f8a5452d3c2725a2c3fcaea5b31619954aed1
Przemek <przemekworld@gmail.com>
2016-02-24 20:15:51 +0100

    Dodanie gitignore

---
+++ AutoBlogProgramistyPosts/AutoBlogProgramistyPosts/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBlogProgramistyPosts
+{
+    public class Post
+    {
+        public string HtmlMsg { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+        public IEnumerable<string> Category { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
---
+++ AutoBlogProgramistyPosts/AutoBlogProgramistyPosts/PostsSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBlogProgramistyPosts
+{
+    public class PostsSender 
+    {
+        public bool PostPost(Post post)
+        {
+            return false ;
+        }
+    }
+}
