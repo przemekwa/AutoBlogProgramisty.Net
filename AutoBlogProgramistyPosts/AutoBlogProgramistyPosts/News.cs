53fe4d92e487e0c7e210aa3240f38f61af266747
Przemek <przemekworld@gmail.com>
2016-02-27 11:09:57 +0100

    Zmiana koncepcji

---
+++ AutoBlogProgramistyPosts/AutoBlogProgramistyPosts/News.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBlogProgramistyPosts
+{
+    public class NewsDto
+    {
+       public string Header { get; set; }
+        public List<LinkDto> Links { get; set; }
+    }
+
+    public class LinkDto
+    {
+        public string Header { get; set; }
+        public string Url { get; set; }
+    }
+}
